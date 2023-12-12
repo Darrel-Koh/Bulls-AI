766bdc9c605e9862b560e359a1693c2919e5ecbc
cccc1414 <mailclaumine@gmail.com>
2023-12-13 00:42:31 +0800

    LoginPage v1

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -8,6 +8,7 @@ import Navbar from "./components/navbar";
 import RecordList from "./components/recordList";
 import Edit from "./components/edit";
 import Create from "./components/create";
+import LoginPage from "./components/loginPage";
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
         <Route exact path="/" element={<RecordList />} />
         <Route path="/edit/:id" element={<Edit />} />
         <Route path="/create" element={<Create />} />
+        <Route path="/loginPage" element={<LoginPage />} />
       </Routes>
       </div>
     </div>
