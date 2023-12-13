d18d1dc83ddd1faddc3c3f1bed0c3df2e6e279fd
boonjuncheng <bjuncheng98@gmail.com>
2023-12-13 22:03:59 +0800

    Login Page fetch function and API
    Create fetch function on login page

---
+++ mern/server/routes/login.mjs
@@ -0,0 +1,22 @@
+import express from "express";
+import db from "../db/conn.mjs";
+import { ObjectId } from "mongodb";
+
+const router = express.Router();
+
+// Check whether email and password exist in database
+router.get("/", async (req, res) => {
+  // Checking users table if email and password exist
+  const userInfo = await db
+    .collection("users")
+    .findOne({ email: req.body.userName, password: req.body.password });
+
+  // if userInfo = null
+  if (!userInfo) {
+    res.send("Not found").status(404);
+
+    // if userInfo exist
+  } else {
+    res.send(userInfo).status(200);
+  }
+});
