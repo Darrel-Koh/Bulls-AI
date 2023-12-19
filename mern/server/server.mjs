d3db5d967f04c80d9b14315874c1a12dc018a120
boonjuncheng <bjuncheng98@gmail.com>
2023-12-19 19:27:45 +0800

    importing users

--- mern/server/server.mjs
+++ mern/server/server.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "./loadEnvironment.mjs";
 import records from "./routes/record.mjs";
+import users from "./routes/login.mjs";
 
 const PORT = 5050;
 const app = express();
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/record", records);
+app.use("/login", users);
 
 // start the Express server
 app.listen(PORT, () => {
