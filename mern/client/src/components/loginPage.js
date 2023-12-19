aa1d308c9b467e1c28f86f8915c8ba5d8ca04f0f
boonjuncheng <bjuncheng98@gmail.com>
2023-12-19 19:35:36 +0800

    change 5000 to 5050

--- mern/client/src/components/loginPage.js
+++ mern/client/src/components/loginPage.js
@@ -1,9 +1,7 @@
-
 import React, { useState } from "react";
-import '../components/style.css';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-
+import "../components/style.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
   const [activeTab, setActiveTab] = useState("login");
@@ -15,165 +13,167 @@ const LoginPage = () => {
   const navigate = useNavigate();
   const [setLoggedIn] = useState(false);
 
-
-  const handleLogin = async () => {
+  const handleLogin = async (e) => {
+    e.preventDefault(); // Prevent the default form submission behavior
     try {
-      const response = await axios.post('http://localhost:5000/login', { loginEmail, loginPassword });
-      console.log('Login successful. Token:', response.data.token);
+      const response = await axios.post(`http://localhost:5050/login`, {
+        email: loginEmail,
+        password: loginPassword,
+      });
+      console.log("Login successful. Token:", response.data.token);
 
       // Set loggedIn state to true and redirect to the main page
       setLoggedIn(true);
-      navigate('/mainPage');
+      navigate("/mainPage");
     } catch (error) {
-      console.error('Login failed:', error.message);
+      console.error("Login failed:", error.message);
     }
   };
 
   const handleRegister = async () => {
     try {
-      await axios.post('http://localhost:5000/register', { registerEmail, registerPassword, registerFirstName });
-      console.log('Registration successful');
+      await axios.post("http://localhost:5000/register", {
+        registerEmail,
+        registerPassword,
+        registerFirstName,
+      });
+      console.log("Registration successful");
     } catch (error) {
-      console.error('Registration failed:', error.message);
+      console.error("Registration failed:", error.message);
     }
   };
 
   return (
-
     <div className="parent-div">
-
-    <div className="outer-div">
-
-    <div
-      style={{
-        textAlign: "center",
-        marginTop: "100px",
-        minHeight: "100vh",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-
-      <div style={{ marginBottom: "20px" }}>
-        <button
+      <div className="outer-div">
+        <div
           style={{
-            padding: "12px 50px",
-            marginTop: "140px",
-            marginRight: "10px",
-            backgroundColor: activeTab === "login" ? "#28a745" : "#007bff",
-            color: "white",
-            border: "2px",
-            borderRadius: "8px",
-            cursor: "pointer",
-            fontSize: "18px",
+            textAlign: "center",
+            marginTop: "100px",
+            minHeight: "100vh",
+            display: "flex",
+            flexDirection: "column",
           }}
-          onClick={() => setActiveTab("login")}
-        >
-          Login
-        </button>
-        <button
-          style={{
-            padding: "12px 40px",
-            marginRight: "10px",
-            backgroundColor: activeTab === "register" ? "#28a745" : "#007bff",
-            color: "white",
-            border: "2px",
-            borderRadius: "8px",
-            cursor: "pointer",
-            fontSize: "18px",
-          }}
-          onClick={() => setActiveTab("register")}
-        >
-          Register
-        </button>
-
-        
-      </div>
-      {activeTab === "login" && (
-        <form
-          onSubmit={handleLogin}
-          style={{ maxWidth: "300px", margin: "auto" }}
         >
           <div style={{ marginBottom: "20px" }}>
-            <label htmlFor="loginEmail">Email Address:</label>
-            <input
-              type="text"
-              id="loginEmail"
-              value={loginEmail}
-              onChange={(e) => setLoginEmail(e.target.value)}
-              style={{ width: "100%", padding: "8px" }}
-            />
-          </div>
-          <div style={{ marginBottom: "15px" }}>
-            <label htmlFor="loginPassword">Password:</label>
-            <input
-              type="password"
-              id="loginPassword"
-              value={loginPassword}
-              onChange={(e) => setLoginPassword(e.target.value)}
-              style={{ width: "100%", padding: "8px" }}
-            />
-          </div>
-
-          <div>
-            <button type="submit" style={submitButtonStyle}>
+            <button
+              style={{
+                padding: "12px 50px",
+                marginTop: "140px",
+                marginRight: "10px",
+                backgroundColor: activeTab === "login" ? "#28a745" : "#007bff",
+                color: "white",
+                border: "2px",
+                borderRadius: "8px",
+                cursor: "pointer",
+                fontSize: "18px",
+              }}
+              onClick={() => setActiveTab("login")}
+            >
               Login
             </button>
-          </div>
-        </form>
-      )}
-      {activeTab === "register" && (
-        <form
-          onSubmit={handleRegister}
-          style={{ maxWidth: "300px", margin: "auto" }}
-        >
-          <div style={{ marginBottom: "15px" }}>
-            <label htmlFor="registerEmail">Email Address:</label>
-            <input
-              type="text"
-              id="registerUsername"
-              value={registerEmail}
-              onChange={(e) => setRegisterEmail(e.target.value)}
-              style={{ width: "100%", padding: "8px" }}
-            />
-          </div>
-          <div style={{ marginBottom: "15px" }}>
-            <label htmlFor="registerPassword">Password:</label>
-            <input
-              type="password"
-              id="registerPassword"
-              value={registerPassword}
-              onChange={(e) => setRegisterPassword(e.target.value)}
-              style={{ width: "100%", padding: "8px" }}
-            />
-          </div>
-          <div style={{ marginBottom: "15px" }}>
-            <label htmlFor="registerFirstName">First Name:</label>
-            <input
-              type="text"
-              id="registerFirstName"
-              value={registerFirstName}
-              onChange={(e) => setRegisterFirstName(e.target.value)}
-              style={{ width: "100%", padding: "8px" }}
-            />
-          </div>
-          <div>
-            <button type="submit" style={submitButtonStyle}>
+            <button
+              style={{
+                padding: "12px 40px",
+                marginRight: "10px",
+                backgroundColor:
+                  activeTab === "register" ? "#28a745" : "#007bff",
+                color: "white",
+                border: "2px",
+                borderRadius: "8px",
+                cursor: "pointer",
+                fontSize: "18px",
+              }}
+              onClick={() => setActiveTab("register")}
+            >
               Register
             </button>
           </div>
-        </form>
-      )}
-      <footer
-        style={{
-          marginTop: "auto",
-          padding: "10px",
-          backgroundColor: "#f4f4f4",
-        }}
-      >
-        <p>&copy; 2023 Bulls Ai. All rights reserved.</p>
-      </footer>
-      </div>
+          {activeTab === "login" && (
+            <form
+              onSubmit={handleLogin}
+              style={{ maxWidth: "300px", margin: "auto" }}
+            >
+              <div style={{ marginBottom: "20px" }}>
+                <label htmlFor="loginEmail">Email Address:</label>
+                <input
+                  type="text"
+                  id="loginEmail"
+                  value={loginEmail}
+                  onChange={(e) => setLoginEmail(e.target.value)}
+                  style={{ width: "100%", padding: "8px" }}
+                />
+              </div>
+              <div style={{ marginBottom: "15px" }}>
+                <label htmlFor="loginPassword">Password:</label>
+                <input
+                  type="password"
+                  id="loginPassword"
+                  value={loginPassword}
+                  onChange={(e) => setLoginPassword(e.target.value)}
+                  style={{ width: "100%", padding: "8px" }}
+                />
+              </div>
+
+              <div>
+                <button type="submit" style={submitButtonStyle}>
+                  Login
+                </button>
+              </div>
+            </form>
+          )}
+          {activeTab === "register" && (
+            <form
+              onSubmit={handleRegister}
+              style={{ maxWidth: "300px", margin: "auto" }}
+            >
+              <div style={{ marginBottom: "15px" }}>
+                <label htmlFor="registerEmail">Email Address:</label>
+                <input
+                  type="text"
+                  id="registerUsername"
+                  value={registerEmail}
+                  onChange={(e) => setRegisterEmail(e.target.value)}
+                  style={{ width: "100%", padding: "8px" }}
+                />
+              </div>
+              <div style={{ marginBottom: "15px" }}>
+                <label htmlFor="registerPassword">Password:</label>
+                <input
+                  type="password"
+                  id="registerPassword"
+                  value={registerPassword}
+                  onChange={(e) => setRegisterPassword(e.target.value)}
+                  style={{ width: "100%", padding: "8px" }}
+                />
+              </div>
+              <div style={{ marginBottom: "15px" }}>
+                <label htmlFor="registerFirstName">First Name:</label>
+                <input
+                  type="text"
+                  id="registerFirstName"
+                  value={registerFirstName}
+                  onChange={(e) => setRegisterFirstName(e.target.value)}
+                  style={{ width: "100%", padding: "8px" }}
+                />
+              </div>
+              <div>
+                <button type="submit" style={submitButtonStyle}>
+                  Register
+                </button>
+              </div>
+            </form>
+          )}
+          <footer
+            style={{
+              marginTop: "auto",
+              padding: "10px",
+              backgroundColor: "#f4f4f4",
+            }}
+          >
+            <p>&copy; 2023 Bulls Ai. All rights reserved.</p>
+          </footer>
+        </div>
       </div>
     </div>
   );
@@ -187,7 +187,7 @@ const submitButtonStyle = {
   borderRadius: "15px",
   cursor: "pointer",
   marginBottom: "100px",
-  fontSize: "18px"
+  fontSize: "18px",
 };
 
 export default LoginPage;
