d6d32ba465c14a508e6f7ea2fdccbf821865b3c6
cccc1414 <mailclaumine@gmail.com>
2024-01-30 16:52:47 +0800

    update

--- mern/server/routes/mainpage.mjs
+++ mern/server/routes/mainpage.mjs
@@ -46,7 +46,7 @@ const fetchDataFromDatabase = async (page, pageSize) => {
 router.get("/", async (req, res) => {
     let collection = await bullsdb.collection("ticker_data");
     // Limit to 632 collections for faster loading process
-    let results = await collection.find({}).limit(50).toArray(); 
+    let results = await collection.find({}).limit(10).toArray(); 
     res.send(results).status(200);
 });
 
