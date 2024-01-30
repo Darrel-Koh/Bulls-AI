e07fbc9d720446a419ccde3e6d1a3a660ded31de
cccc1414 <mailclaumine@gmail.com>
2024-01-30 17:21:05 +0800

    updated merge with jh and boon

--- mern/client/src/components/mainPage.js
+++ mern/client/src/components/mainPage.js
@@ -2,7 +2,6 @@
 //mainPage.js
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 const MainPage = () => {
