f8aa02492c9f9291b8dae11067bfaeb2db2c31ea
boonjuncheng <bjuncheng98@gmail.com>
2024-02-13 12:30:56 +0800

    forgetpassword without logging in

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -1,9 +1,7 @@
-//App.js
+// App.js
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-// eslint-disable-next-line no-unused-vars
+import { Route, Routes, Navigate, useNavigate } from "react-router-dom";
 import Navbar from "./components/navbar";
-
 import Edit from "./components/edit2";
 import Header from "./components/header";
 import GlossaryPage from "./components/Glossarypage";
@@ -12,40 +10,44 @@ import AddTickerPage from "./components/addtickerpage";
 import ViewTickers from "./components/viewTickers";
 import Create from "./components/create2";
 import LoginPage from "./components/loginPage";
-import ForgetPassword from "./components/ForgetPassword";
 import MainPage from "./components/mainPage";
 import UserList from "./components/recordList2";
 import { useState } from "react";
 import AuthContext from "./components/AuthContext";
-import ChangePassword from "./components/ChangePassword"; // Import the new ChangePassword component
+import ChangePassword from "./components/ChangePassword";
+import ForgetPassword from "./components/ForgetPassword";
 
 const App = () => {
   const [userId, setUserId] = useState(null);
   const [userName, setUserName] = useState("");
+
   return (
-    <div>
-      <AuthContext.Provider
-        value={{ userId, setUserId, userName, setUserName }}
-      >
-        <Header />
-        <div style={{ margin: 20 }}>
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/edit/:id" element={<Edit />} />
-            <Route path="/create" element={<Create />} />
-            <Route path="/glossary" element={<GlossaryPage />} />
-            <Route path="/my-ticker" element={<MyTickerPage />} />
-            <Route path="/add-ticker" element={<AddTickerPage />} />
-            <Route path="/recordListPage" element={<UserList />} />
-            <Route path="/viewTickers" element={<ViewTickers />} />
-            <Route path="/forget-password" element={<ForgetPassword />} />
-            <Route path="/mainPage" element={<MainPage />} />
-            <Route path="/changepassword" element={<ChangePassword />} />{" "}
-            {/* Add this line */}
-          </Routes>
-        </div>
-      </AuthContext.Provider>
-    </div>
+    <AuthContext.Provider value={{ userId, setUserId, userName, setUserName }}>
+      {userId && <Header />}
+      <div style={{ margin: 20 }}>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/edit/:id" element={<Edit />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/glossary" element={<GlossaryPage />} />
+          <Route path="/my-ticker" element={<MyTickerPage />} />
+          <Route path="/add-ticker" element={<AddTickerPage />} />
+          <Route path="/recordListPage" element={<UserList />} />
+          <Route path="/viewTickers" element={<ViewTickers />} />
+          <Route path="/mainPage" element={<MainPage />} />
+          <Route path="/changepassword" element={<ChangePassword />} />
+          <Route
+            path="/forget-password"
+            element={<ForgetPassword userId={userId} />}
+          />
+          {/* Add a default route for unmatched paths */}
+          <Route
+            path="*"
+            element={<Navigate to={userId ? "/mainPage" : "/"} replace />}
+          />
+        </Routes>
+      </div>
+    </AuthContext.Provider>
   );
 };
 
