4d137cd336f8bbf90d77de9013b0f8686fa50908
jordanwong03 <jordanwong03@gmail.com>
2024-02-14 16:19:47 +0800

    connected frontend to backend to retrieve search

--- mern/client/src/components/viewTickers.js
+++ mern/client/src/components/viewTickers.js
@@ -9,87 +9,60 @@ const ViewTickers = () => {
   const navigate = useNavigate();
   const { searchResults, searchTerm } = location.state || {};
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 10; // Adjust the number of items per page as needed
+  const itemsPerPage = 10;
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
   const [relatedNews, setRelatedNews] = useState([]);
   const [loadingNews, setLoadingNews] = useState(true);
 
- // News Section 
-useEffect(() => {
-  console.log('Fetching related news for:', searchTerm);
-  const apiKey = '466de00744114cc8be03ca388f1f816f'; // Replace with your actual News API key
+  useEffect(() => {
+    console.log('Fetching related news for:', searchTerm);
+    const apiKey = '466de00744114cc8be03ca388f1f816f';
 
-  const fetchRelatedNews = async () => {
-    try {
-      const stocksKeywords = ['stocks', 'financial', 'market', 'finance']; // Add more keywords as needed
-      const searchTermWithKeywords = `${searchTerm} ${stocksKeywords.join(' OR ')}`;
+    const fetchRelatedNews = async () => {
+      try {
+        const stocksKeywords = ['stocks', 'financial', 'market', 'finance'];
+        const searchTermWithKeywords = `${searchTerm} ${stocksKeywords.join(' OR ')}`;
+        const url = `https://newsapi.org/v2/everything?q=${searchTermWithKeywords}&apiKey=${apiKey}&language=en&sortBy=publishedAt&sources=bbc-news`;
 
-      // Construct the URL with language and sortBy parameters
-      const url = `https://newsapi.org/v2/everything?q=${searchTermWithKeywords}&apiKey=${apiKey}&language=en&sortBy=publishedAt&sources=bbc-news`;
+        const response = await fetch(url);
 
-      const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
+        const data = await response.json();
+
+        const limitedNews = data.articles.slice(0, 3);
+
+        setRelatedNews(limitedNews);
+      } catch (error) {
+        console.error('Error fetching related news:', error);
+      } finally {
+        console.log('Finished fetching related news');
+        setLoadingNews(false);
       }
+    };
 
-      const data = await response.json();
-
-      // Limit the news to 3 articles
-      const limitedNews = data.articles.slice(0, 3);
-
-      setRelatedNews(limitedNews);
-    } catch (error) {
-      console.error('Error fetching related news:', error);
-    } finally {
-      console.log('Finished fetching related news');
-      setLoadingNews(false);
+    if (searchTerm) {
+      fetchRelatedNews();
     }
+  }, [searchTerm]);
+
+  useEffect(() => {
+    console.log('Search Term:', searchTerm);
+    console.log('Search Results:', searchResults);
+  }, [searchResults, searchTerm]);
+
+  const handleAddToFavourite = (recordId) => {
+    console.log(`Added record with ID ${recordId} to favourites`);
   };
 
-  // Fetch related news when the component mounts or when the search term changes
-  if (searchTerm) {
-    fetchRelatedNews();
-  }
-}, [searchTerm]);
-
-// search
-useEffect(() => {
-  console.log('Search Term:', searchTerm);
-  console.log('Search Results:', searchResults);
-}, [searchResults, searchTerm]);
-
-// Filter data based on the search term
-// const filteredData = searchResults.filter((result) =>
-// result.trading_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-// result.symbol.toLowerCase().includes(searchTerm.toLowerCase())
-// );
-// const filteredData = searchResults.filter((result) => {
-//   const tradingName = result?.trading_name?.toLowerCase() || ''; // Check if trading_name is defined
-//   const symbol = result?.symbol?.toLowerCase() || ''; // Check if symbol is defined
-
-//   return tradingName.includes(searchTerm.toLowerCase()) || symbol.includes(searchTerm.toLowerCase());
-// });
-
-const filteredData = Array.isArray(searchResults) ? searchResults.filter((result) => {
-  const tradingName = result?.trading_name?.toLowerCase() || ''; // Check if trading_name is defined
-  const symbol = result?.symbol?.toLowerCase() || ''; // Check if symbol is defined
-
-  return tradingName.includes(searchTerm.toLowerCase()) || symbol.includes(searchTerm.toLowerCase());
-}) : [];
-
-
-const displayedData = filteredData.slice(startIndex, endIndex);
-
-const handleAddToFavourite = (recordId) => {
-  // Implement the logic to add the record to the favourite list
-  console.log(`Added record with ID ${recordId} to favourites`);
-};
-  
-  // Displays search table
   const renderTable = () => {
-    if (displayedData.length === 0) {
+    // Wrap the single object in an array if it's not already an array
+    const dataArray = Array.isArray(searchResults) ? searchResults : [searchResults];
+  
+    if (dataArray.length === 0) {
       return <Typography variant="body1" style={{ marginTop: '10px' }}>No data available for the search term: {searchTerm}</Typography>;
     }
 
@@ -107,7 +80,7 @@ const handleAddToFavourite = (recordId) => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {displayedData.map((result, index) => (
+            {dataArray.map((result, index) => (
               <TableRow key={result._id} style={{ background: index % 2 === 0 ? grey[200] : 'white' }}>
                 <TableCell style={tableCellStyle}>{result.trading_name}</TableCell>
                 <TableCell style={tableCellStyle}>{result.symbol}</TableCell>
@@ -135,35 +108,31 @@ const handleAddToFavourite = (recordId) => {
 
   const renderRelatedNews = () => {
     return (
-      
       <div style={{ marginTop: '50px' }}>
-      <Typography variant="h6" style={{ marginBottom: '20px' }}>Related News</Typography>
-      {loadingNews ? (
-        <CircularProgress />
-      ) : relatedNews.length > 0 ? (
-        <ul style={{ listStyle: 'none', padding: 0 }}>
-          {relatedNews.map((article, index) => (
-            <li key={index} style={{ marginBottom: '20px', border: '1px solid #ddd', padding: '10px', borderRadius: '8px' }}>
-              <Typography variant="h6">{article.title}</Typography>
-              <Typography variant="body1" style={{ margin: '10px 0 0' }}>{article.description}</Typography>
-              <Link href={article.url} target="_blank" rel="noopener noreferrer" style={{ display: 'block', marginTop: '10px', color: '#007BFF', textDecoration: 'underline' }}>
-                Read More
-              </Link>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <Typography variant="body1">No related news available.</Typography>
-      )}
-    </div>
-    
+        <Typography variant="h6" style={{ marginBottom: '20px' }}>Related News</Typography>
+        {loadingNews ? (
+          <CircularProgress />
+        ) : Array.isArray(relatedNews) && relatedNews.length > 0 ? (
+          <ul style={{ listStyle: 'none', padding: 0 }}>
+            {relatedNews.map((article, index) => (
+              <li key={index} style={{ marginBottom: '20px', border: '1px solid #ddd', padding: '10px', borderRadius: '8px' }}>
+                <Typography variant="h6">{article.title}</Typography>
+                <Typography variant="body1" style={{ margin: '10px 0 0' }}>{article.description}</Typography>
+                <Link href={article.url} target="_blank" rel="noopener noreferrer" style={{ display: 'block', marginTop: '10px', color: '#007BFF', textDecoration: 'underline' }}>
+                  Read More
+                </Link>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <Typography variant="body1">No related news available.</Typography>
+        )}
+      </div>
     );
   };
 
-
   return (
     <div>
-      {/* Add to Favourite Button */}
       <div style={{ marginTop: '100px', display: 'flex', justifyContent: 'flex-end', alignItems: 'center', marginBottom: '20px' }}>
         <Button onClick={() => navigate('/mainPage')} variant="contained" style={returnButtonStyle}>
           Return to Main Page
@@ -173,7 +142,6 @@ const handleAddToFavourite = (recordId) => {
       {renderTable()}
       {searchTerm && renderRelatedNews()}
 
-      {/* Footer */}
       <footer className="footer">
         <Typography variant="body2">&copy; 2023 Bulls Ai. All rights reserved.</Typography>
       </footer>
@@ -182,7 +150,7 @@ const handleAddToFavourite = (recordId) => {
 };
 
 const favouriteButtonStyle = {
-  backgroundColor: '#FFD700', // Yellow color for the favourite button
+  backgroundColor: '#FFD700',
   color: 'black',
   padding: '8px 12px',
   borderRadius: '4px',
