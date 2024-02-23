1aa7579e7f0e260fa735a900d6f5a8d1b22d2af2
Darrel-koh <koh465@gmail.com>
2024-02-23 19:15:33 +0800

    can retrieve image from search

--- mern/server/routes/searchLor.mjs
+++ mern/server/routes/searchLor.mjs
@@ -1,19 +1,27 @@
 import express from "express";
-// import db from "../db/conn.mjs";
-import {db, bullsdb} from "../db/conn.mjs"
+import { db, bullsdb } from "../db/conn.mjs";
 import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
-
 router.get("/:tickername", async (req, res) => {
-    let collection = await bullsdb.collection("ticker_data");
-    let query = {trading_name : req.params.tickername}; // Changed this line
-    let result = await collection.findOne(query);
-  
-    if (!result) res.status(404).send("Not found"); // Changed this line
-    else res.status(200).send(result); // Changed this line
-});
+    try {
+        const collection = bullsdb.collection("ticker_data");
+        const query = { trading_name: req.params.tickername };
+        const result = await collection.findOne(query);
 
+        if (!result || !result.plot_data) {
+            res.status(404).json({ message: "Plot data not found" });
+            return;
+        }
+
+        // Send the plot data as a response
+        res.set("Content-Type", "image/png");
+        res.send(result.plot_data.buffer);
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
 
 export default router;
